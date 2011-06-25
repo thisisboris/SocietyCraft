585610f9b019bf354dda211d0ce928db7c878e0c
cskiwi <rylkiwi@gmail.com>
2011-06-25 22:11:28 +0200

    Fixed issue when player leaving the game not beeing deleted from the online player list.

--- src/be/thisisboris/SocietyCraft/SCPlayerListener.java
+++ src/be/thisisboris/SocietyCraft/SCPlayerListener.java
@@ -1,5 +1,6 @@
 package be.thisisboris.SocietyCraft;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -26,12 +27,20 @@ public class SCPlayerListener extends PlayerListener {
 	
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		int index = plugin.Onlineplayerlist.size();
-		
 		plugin.Onlineplayerlist.add(event.getPlayer());
-		SCLogger.warning("Adding "+event.getPlayer().getDisplayName()+" to the list");
 	}
+	
 	public void onPlayerQuit(PlayerQuitEvent event) {
-		 
+		int lengthList = plugin.Onlineplayerlist.size();
+		Player QuitingPlayer = event.getPlayer();
+		for (int i = 0; i < lengthList; i++){
+			Player TempPlayer = plugin.Onlineplayerlist.get(i);
+			if (TempPlayer.getDisplayName() == QuitingPlayer.getDisplayName()){
+				plugin.Onlineplayerlist.remove(i);
+				break;
+			}
+		}
+	
 	}    	
     public void onPlayerKick(PlayerKickEvent event) {
     	
